02253d22466a282b8e9e73abd9df0a4234a47a64
Bálint Kocsis <kocsisbalint999@gmail.com>
2021-05-24 01:32:36 +0200

    Main program.

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,52 @@
 
+-- | The main program.
 module Main where
 
+import SFPL.Base
+import SFPL.Elab
+import SFPL.Eval
+import SFPL.Parser
+import SFPL.Syntax.Core
+import qualified SFPL.Syntax.Raw as R
+import SFPL.Utils
+import System.Environment
+import System.Exit
+import System.IO
+import Text.Printf
+
+displayHelpMessage :: IO ()
+displayHelpMessage = do
+  progName <- getProgName
+  printf   "usage: %s [--help | <file>]\n" progName
+  putStrLn "  --help:    display this help message"
+  putStrLn "  <file>:    name of the source file"
+
+parseSrc :: String -> String -> IO R.Program
+parseSrc fileName src = do
+  let res = parseProgram fileName src
+  case res of
+    Left bundle -> putStr (showPretty' bundle) >> exitSuccess
+    Right prog  -> pure prog
+
+elab :: SourceFile -> R.Program -> IO (EvalCxt, Lvl)
+elab src prog = do
+  let (res, metas) = elabProgram prog
+  case res of
+    Left errors -> putStr (showPretty (src, metas) errors) >> exitSuccess
+    Right x     -> pure x
+
+interpret :: String -> String -> IO ()
+interpret fileName src = do
+  rawProgram <- parseSrc fileName src
+  let src' = arr $ lines src
+  (cxt, mainLvl) <- elab src' rawProgram
+  runMain cxt mainLvl
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  args <- getArgs
+  case args of
+    ["--help"]  -> displayHelpMessage
+    [fileName]  -> interpret fileName =<< readFile fileName
+    _           -> displayHelpMessage
+
--- app/SFPL/Parser.hs
+++ app/SFPL/Parser.hs
@@ -9,7 +9,21 @@ module SFPL.Parser
     dataDecl,
     typeDecl,
     program,
+    
+    -- * Helpers
+    parseProgram
   )
   where
 
+import Data.Void
 import SFPL.Parser.Internal
+import SFPL.Syntax.Raw
+import Text.Megaparsec
+
+-- | Parse a complete program given the name
+-- and contents of the source file.
+--
+-- @since 1.0.0
+parseProgram ::
+  String -> String -> Either (ParseErrorBundle String Void) Program
+parseProgram = parse (topLevel program)
